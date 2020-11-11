09ec411fb451dc757f2fd9f1ce4b1b530cdeb733
tomrodenhagen <rodenhag@gmail.com>
2020-11-11 12:13:50 +0100

    Remove unfinished code

--- src/etas.py
+++ src/etas.py
@@ -13,10 +13,3 @@ def get_kernel(p, m0):
 
     return kernel
 
-
-
-def get_kernel_integral(p, m0 , t_start, t_end ):
-    g_integral = lambda t: - (t + p["c"]) / ( p["c"] * ( (t + p["c"]) / p["c"] )** (p["p"]) )
-    sigma = lambda m: p["D"] * np.exp(p["gamma"] * (m - m0))
-    f_integral = lambda x, y, m: (p["q"] - 1) / (np.pi * sigma(m)) * (1 + (x ** 2 + y ** 2) / sigma(m)) ** (-p["q"])
-    k = lambda m: p["A"] * np.exp(p["alpha"] * (m - m0))
